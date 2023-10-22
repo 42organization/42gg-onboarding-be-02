a46febc791660c5f40c5ec5ca28e6eee2181dbf4
sgo <ko9603@naver.com>
2023-10-22 23:17:31 +0900

    feat: Post Dto 생성

---
+++ sgo/onboarding2th/dto/PostRequestDto.java
@@ -0,0 +1,16 @@
+package gg.onboarding2th.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostRequestDto {
+
+    private String title;
+    private String content;
+}
