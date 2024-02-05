35335916ce692e06c2e843ea193ff322fbeae296
gkwon <gkwon@student.42seoul.kr>
2024-02-05 14:50:03 +0900

    [REFACTORING]pr feedback

--- crud_project/src/main/resources/CreateTable.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE CRUD (
-                      id INT PRIMARY KEY AUTO_INCREMENT,
-                      title VARCHAR(255) NOT NULL,
-                      content TEXT NOT NULL
-);
--- gkwon/crud_project/src/main/resources/CreateTable.sql
+++ gkwon/crud_project/src/main/resources/CreateTable.sql
@@ -1,3 +1,5 @@
+drop table if exists CRUD;
+
 CREATE TABLE CRUD (
                       id INT PRIMARY KEY AUTO_INCREMENT,
                       title VARCHAR(255) NOT NULL,
